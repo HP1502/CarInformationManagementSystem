6e54c5f80de3c52e7f1019ca6483267e32992841
Kevin <balankev@gmail.com>
2024-09-09 17:30:41 +0530

    corrected mistakes

---
+++ CarManagement/new files/migrations/20240909114940_InitialCreate.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CarTransmissionType",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarTransmissionType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CarType",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Manufacturer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    ContactNo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RegisteredOffice = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Manufacturer", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CAR",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Model = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    ManufacturerId = table.Column<int>(type: "int", nullable: false),
+                    TypeId = table.Column<int>(type: "int", nullable: false),
+                    Engine = table.Column<string>(type: "nvarchar(4)", maxLength: 4, nullable: false),
+                    BHP = table.Column<int>(type: "int", nullable: false),
+                    TransmissionId = table.Column<int>(type: "int", nullable: false),
+                    Mileage = table.Column<int>(type: "int", nullable: false),
+                    Seat = table.Column<int>(type: "int", nullable: false),
+                    AirBagDetails = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    BootSpace = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CAR", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CAR_CarTransmissionType_TransmissionId",
+                        column: x => x.TransmissionId,
+                        principalTable: "CarTransmissionType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CAR_CarType_TypeId",
+                        column: x => x.TypeId,
+                        principalTable: "CarType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CAR_Manufacturer_ManufacturerId",
+                        column: x => x.ManufacturerId,
+                        principalTable: "Manufacturer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CAR_ManufacturerId",
+                table: "CAR",
+                column: "ManufacturerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CAR_TransmissionId",
+                table: "CAR",
+                column: "TransmissionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CAR_TypeId",
+                table: "CAR",
+                column: "TypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CAR");
+
+            migrationBuilder.DropTable(
+                name: "CarTransmissionType");
+
+            migrationBuilder.DropTable(
+                name: "CarType");
+
+            migrationBuilder.DropTable(
+                name: "Manufacturer");
+        }
+    }
+}
