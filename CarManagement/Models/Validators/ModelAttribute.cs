88eac82cbc7e90fd6a90fc7c710ee8208f6cf91f
Kevin <balankev@gmail.com>
2024-09-10 12:42:07 +0530

    Add files via upload

---
+++ CarManagement/Models/Validators/ModelAttribute.cs
@@ -0,0 +1,22 @@
+﻿using CarManagement.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarManagement.Models.Validators
+{
+    public class ModelAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var model = value as string;
+            CarInformationSystemContext c = new CarInformationSystemContext();
+
+            if (c.CAR.Any(c => c.Model == model))
+            {
+                return new ValidationResult("Model name must be unique.");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
