b626e9d3a3fc80ecdb152d2df8f1be31a62053e9
Jeevanandham p <130121911+jeevanandham-p@users.noreply.github.com>
2024-09-10 12:14:44 +0530

    Add files via upload

---
+++ CarManagement/Models/ICarRepository.cs
@@ -0,0 +1,19 @@
+﻿using CarManagement.Data;
+
+namespace CarManagement.Models
+{
+    public interface ICarRepository
+    {
+        public Car SearchCar(CarInformationSystemContext context, string? model);
+        public bool AddCar(CarInformationSystemContext context,Car car);
+        public bool ModifyCar(CarInformationSystemContext context, Car updatedCar, string model);
+        public bool RemoveCar(CarInformationSystemContext context, string model);
+
+        //public class CarView;
+        public IEnumerable<object> GetCarsWithDetails(CarInformationSystemContext context, string manufacturerName);
+
+
+
+
+    }
+}
